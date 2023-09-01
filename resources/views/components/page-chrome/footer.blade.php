0069b2c6ef9add35027b144061e6d652c5ce773e
Rachel Vecchitto <rachel@magnetbox.org>
2023-08-31 21:24:32 -0600

    footer tweaks

--- resources/views/components/page-chrome/footer.blade.php
+++ resources/views/components/page-chrome/footer.blade.php
@@ -1,10 +1,11 @@
-<div class="bg-blue border-t-2 border-teal py-10 text-white text-sm">
-    <div class="container flex flex-col items-center space-y-8">
-        <div class="space-y-1 text-center">
+<div class="bg-blue border-t-2 border-teal py-10 text-white">
+    <div class="container flex flex-col items-center">
+        <div class="space-y-2 text-center">
             <p>&copy; {{ date('Y') }} <a class="text-white hover:text-white underline" target="_blank" href="https://www.winterfactory.com">Winter Factory, LLC</a></p>
             <p>Built with 🩵 and ❄️ in Colorado</p>
         </div>
-        <div class="text-center text-sm">
+
+        <div class="text-center text-sm mt-10">
             <span class="sm:hidden md:hidden lg:hidden xl:hidden">xs</span>
             <span class="hidden sm:inline md:hidden">sm</span>
             <span class="hidden md:inline lg:hidden">md</span>
